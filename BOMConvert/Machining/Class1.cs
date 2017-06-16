1a417b5899c47294af94c0f9dd4e3b9028b140a0
盛章 刘 <liuszhang@qq.com>
2017-06-16 17:10:30 +0800

    New BOM Convert Project

---
+++ BOMConvert/Machining/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Machining
+{
+    public class Class1
+    {
+    }
+}
